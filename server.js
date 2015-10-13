7bea13e3a3acb9a223c3bbd8e50fb00462ac3a07
ntoscano <Nicolas.Toscano@gmail.com>
2015-10-12 18:47:37 -0700

    fixed

--- server.js
+++ server.js
@@ -3,10 +3,5 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 
-var server = app.listen(port, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('App listening at http://%s:%s', host, port);
-});
 app.use(express.static(__dirname + '/app'));
+app.listen(port);
